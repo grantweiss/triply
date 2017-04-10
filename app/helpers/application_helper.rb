fe2b32b30ebee97e53135b71dd28529d8d0db3e6
Grant <grant.sweiss@gmail.com>
2017-04-10 12:26:02 -0500

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #return full title on a per-page basis.
+  def full_title(page_tile = '')
+  	base_title = "triply adventure helper"
+  	if page_tile.empty?
+  		base_title
+  	else
+  		page_tile + " | " + base_title
+  	end
+  end
 end
